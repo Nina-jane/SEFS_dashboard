5a017dd35ad2251fc2d2a0822ac8cf8a6d53d4b1
Nina-jane <ninajaneives@hotmail.com>
2024-11-26 10:08:40 +1300

    Update app.py

--- app.py
+++ app.py
@@ -67,14 +67,15 @@ app.layout = html.Div([
 #     else:
 #         return about_page
         
-# external_stylesheets = [
-#     'https://codepen.io/chriddyp/pen/bWLwgP.css',
-#     {
-#         'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
-#         'rel': 'stylesheet',
-#         'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
-#         'crossorigin': 'anonymous'
-#     }
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
 
 
 if __name__ == "__main__":
