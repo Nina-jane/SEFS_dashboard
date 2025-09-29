33201d2484b0a042c27e4aae743a22075993d364
Nina-jane <ninajaneives@hotmail.com>
2025-09-29 21:21:50 +1300

    updating methods page

--- pages/methods_&_calculations.py
+++ pages/methods_&_calculations.py
@@ -71,23 +71,40 @@ layout = html.Div([
                     children=[
                         dmc.AccordionItem(
                             [
-                                dmc.AccordionControl("For answering RQ1"),
+                                dmc.AccordionControl("For generating countries\' contributions to warming (answering RQ1)"),
                                 dmc.AccordionPanel(
-                                    "To calculate countries' contributions to historical global warming, code from the Callahan & Mankin (2022) study " \
-                                    "was modified to get countries' warming contributions from individual greenhouse gases. This was done by running the " \
-                                    "python scripts with and without countries' individual GHG emissions. Then, the overall differences in global warming " \
-                                    "between when countries' GHG emissions were included and excluded was calculated. The steps involved with this are detailed below. "
+                                    "Countries' contributions to historical global warming were calculated by building on code and datasets from the Callahan & Mankin (2022) study. " \
+                                    "The code was modified to get countries' warming contributions from three individual climate forcers: carbon dioxide (CO\u2082), " \
+                                    "methane (CH\u2084) and nitrogen oxides (NOx), where NOx = NO + NO\u2082. The python scripts were run with and without emissions of from these individual climate forcers " \
+                                    "for each country. Then, overall differences in global warming between when countries' climate forcer emissions were included and excluded were calculated. " \
+                                    "Warming differences were attributed to individual countries." \
+                                    #"The steps involved with this are detailed below. " \
+                                    #"Step 1: Calculate global warming with all countries and their climate forcer emissions present. " \
+                                    #"Step 2: Look through each country and each gas and calculate the global warming amount in the absence of that country and that gas. " \
                                 ),
                             ],
                             value="RQ1",
                         ),
                         dmc.AccordionItem(
                             [
-                                dmc.AccordionControl("For answering RQ2"),
+                                dmc.AccordionControl("For generating countries\' consumption-based emissions"),
+                                dmc.AccordionPanel(
+                                    "The environmentally extended input-output analysis (EEIOA) technique was used to calculate countries' consumption-based emissions. " \
+                                    "This research followed the IOA steps outlined in Kitzes (2013). " \
+                                    "An industry-by-industry transaction matrix, Z, was used alongside a matrix of satellite accounts, Q. " \
+                                    "By performing a series of matrix algebra operations on these matrices, including generating the Leontief inverse, " \
+                                    "countries' direct and indirect consumption-based emissions were calculated. " \
+                                    "This information was used to apply emissions- and warming-dependent principles to answer RQs 2 and 3. " \
+                                ),
+                            ],
+                            value="consumption-based emissions",
+                        ),
+                        dmc.AccordionItem(
+                            [
+                                dmc.AccordionControl("For generating distributions of climate finance (answering RQ2)"),
                                 dmc.AccordionPanel(
                                     children=[
-                                    "To calculate countries' shares of a collective climate mitigation finance goal, several ethical principles were applied. Equations 1...6 below " \
-                                    "show the calculations that were performed to apply each of the principles. ",
+                                    "To calculate countries' shares of a collective climate mitigation finance goal several ethical principles were applied according to the equations below. ",
                                     html.H4('Polluter Pays equations'),
                                     dcc.Markdown('$W_{\\text{warming}_T} = \sum_{y=1850}^{2014} \sum_{i=1}^{174} C_{\\text{warming}_{iy}}, \quad T = \\text{choice of the range of } y \\tag{3}$', mathjax=True, style={'textAlign': 'left'}),
                                     dcc.Markdown('$C\\text{Share}_{iT} = \\frac{C_{\\text{warming}_{iT}}}{W_{\\text{warming}_T}} \cdot F_{\\text{Goal}}, \quad F_{\\text{Goal}} \in \{100,\ 50,\ 25,\ 20,\ 15\} \\tag{4}$', mathjax=True, style={'textAlign': 'left'}),
@@ -104,19 +121,17 @@ layout = html.Div([
                         ),
                         dmc.AccordionItem(
                             [
-                                dmc.AccordionControl("For answering RQ3"),
+                                dmc.AccordionControl("For generating warming rights distributions (answering RQ3)"),
                                 dmc.AccordionPanel(
                                     children=[
-                                    "To calculate countries' shares of a collective global emissions budget, several ethical principles were applied. Equations 7...15 below " \
-                                    "show the calculations that were performed to apply each of the principles. "
-                                    "Grandfathering equations ",
+                                    "To calculate countries' shares of a global warming limit two ethical principles were applied according to the equations below. ",
                                     html.H4('Grandfathering equations'),
-                                    dcc.Markdown('$E_T = \sum_{y=1990}^{2015} \sum_{i=1}^{189} C_{iy}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
-                                    dcc.Markdown('$CShare_{iT} = \\frac{C_{iT}}{E_T}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$W_T = \sum_{y=1850}^{2014} \sum_{i=1}^{174} C_{iy}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$CShare_{iT} = \\frac{C_{iT}}{W_T}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
                                     dcc.Markdown('$CShare_{iF} = CShare_{iT} \\times BGoal$', mathjax=True, style={'textAlign': 'left'}),
                                     html.H4('Equality-over-time equations'),
                                     dcc.Markdown('$Personyears_{world} = Personyears_{historical} + Personyears_{future}$', mathjax=True, style={'textAlign': 'left'}),
-                                    dcc.Markdown('$p_{wTS} = \sum_{y=1850}^{2015} \sum_{i=1}^{174} P_{iy} + \sum_{y=2015}^{2050} \sum_{i=1}^{174} P_{iyS}, \quad S = \{SSP1, SSP2, SSP3, SSP4, SSP5\}$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$p_{wTS} = \sum_{y=1850}^{2014} \sum_{i=1}^{174} P_{iy} + \sum_{y=2015}^{2050} \sum_{i=1}^{174} P_{iyS}, \quad S = \{SSP1, SSP2, SSP3, SSP4, SSP5\}$', mathjax=True, style={'textAlign': 'left'}),
                                     dcc.Markdown('$W_T = \sum_{y=1850}^{2014} \sum_{i=1}^{174} C_{iy}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
                                     dcc.Markdown('$I_{\\text{avg}} = \\frac{1.5 \cdot W_T}{p_{wTS}}$', mathjax=True, style={'textAlign': 'left'}),
                                     dcc.Markdown('$p_{iT} = \\left( \sum_{y=1850}^{2014} P_{iy} + \sum_{y=2015}^{2050} P_{iyS} \\right)$', mathjax=True, style={'textAlign': 'left'}),
@@ -124,7 +139,29 @@ layout = html.Div([
                                     ],
                                 ),
                             ],
-                            value="RQ3",
+                            value="RQ3 - warming rights",
+                        ),
+                        dmc.AccordionItem(
+                            [
+                                dmc.AccordionControl("For generating emissions rights distributions (answering RQ3)"),
+                                dmc.AccordionPanel(
+                                    children=[
+                                    "To calculate countries' shares of a global emissions budget two ethical principles were applied according to the equations below. ",
+                                    html.H4('Grandfathering equations'),
+                                    dcc.Markdown('$E_T = \sum_{y=1990}^{2015} \sum_{i=1}^{189} C_{iy}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$CShare_{iT} = \\frac{C_{iT}}{E_T}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$CShare_{iF} = CShare_{iT} \\times BGoal$', mathjax=True, style={'textAlign': 'left'}),
+                                    html.H4('Equality-over-time equations'),
+                                    dcc.Markdown('$Personyears_{world} = Personyears_{historical} + Personyears_{future}$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$p_{wTS} = \sum_{y=1990}^{2015} \sum_{i=1}^{189} P_{iy} + \sum_{y=2016}^{2050} \sum_{i=1}^{189} P_{iyS}, \quad S = \{SSP1, SSP2, SSP3, SSP4, SSP5\}$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$E_T = \sum_{y=1990}^{2015} \sum_{i=1}^{189} C_{iy}, \quad T = \\text{choice of the range of } y$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$J_{\\text{avg}} = \\frac{1.5 \cdot E_T}{p_{wTS}}$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$p_{iT} = \\left( \sum_{y=1990}^{2015} P_{iy} + \sum_{y=2016}^{2050} P_{iyS} \\right)$', mathjax=True, style={'textAlign': 'left'}),
+                                    dcc.Markdown('$CShare_{iF} = J_{\\text{avg}} \cdot p_{iT} - \sum_{y=1990}^{2015} C_{iy}$', mathjax=True, style={'textAlign': 'left'}),
+                                    ],
+                                ),
+                            ],
+                            value="RQ3 - emissions rights",
                         ),
                     ],
                 ),
